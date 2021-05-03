fdebb952eb5a9fc53564efac1dd62ec9c7aaeaf8
Gabriel <gabrielnieton966@gmail.com>
2021-05-03 15:09:44 -0300

    src/store/index,js - Configuração e inclusão do plugin: VuexPersistence

--- vue-chat/src/store/index.js
+++ vue-chat/src/store/index.js
@@ -1,7 +1,17 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
+import mutations from './mutations';
+import actions from './actions';
+
 
 Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV != 'production';
+
+const vuexlocal = new VuexPersistence({
+  storange: window.localStorage,
+});
 
 export default new Vuex.Store({
   state: {
